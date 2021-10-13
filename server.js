3ed56ec66f23d586204d9705a49d40f8341dca7f
Shubham Thorat <thoratshubham048@gmail.com>
2021-10-13 16:42:53 +0530

    make it better

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 import express from 'express'
 import dotnev from 'dotenv'
 import router from './routes/router.js'
-import mailrouter from './routes/mailRouter.js'
-import dbrouter from './routes/dbRouter.js'
-import test from './routes/test.js'
-import exam from './routes/examRouter.js'
-import progress from './routes/progressRouter.js'
 import cors from 'cors'
 import DB from './models/db.js'
 
@@ -22,12 +17,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to language Buddy')
 })
 
-//routing
-app.use('/mail', mailrouter);
-app.use('/db', dbrouter);
-app.use('/db/test', test)
-app.use('/exam', exam)
-app.use('/progress', progress)
+app.use('/', router);
 
 
 
